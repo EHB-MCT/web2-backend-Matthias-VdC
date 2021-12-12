c9385b27ce57dc225b4072582738abe8a2ba2037
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-12-12 11:49:13 +0100

    GET  added & heroku app launched

--- index.js
+++ index.js
@@ -37,36 +37,66 @@ app.get('/', (req, res) => {
     res.status(300).redirect('/api-info.html');
 })
 
-// Add UserData
-app.post('/challenges/send', async (req, res) => {
-    if (!req.body.name || !req.body.points || !req.body.course || !req.body.session) {
-        res.status(400).send("Bad request, missing: id, name, points, course or session!");
+
+//Get userData
+app.get('/userdata/get', async (req, res) => {
+    try {
+        //connect to the database
+        await client.connect();
+        const db = client.db(dbName);
+
+        // Use the collection "Session7"
+        const col = db.collection("userData");
+
+        // Find document
+        const myDoc = await col.find({}).toArray();
+
+        // Print to the console
+        console.log(myDoc);
+
+        //Send back the data with the response
+        res.status(200).send(myDoc);
+
+    } catch (err) {
+        console.log(err.stack);
+        res.status(500).send({
+            error: 'Something went wrong',
+            value: error
+        });
+    } finally {
+        await client.close();
+    }
+})
+
+
+//Add UserData
+app.post('/userdata/send', async (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        res.status(400).send("Bad request, missing: email or password!");
         return;
     }
 
     try {
         await client.connect();
-        const challengeCollect = client.db(dbName).collection("userData");
+        const dataCollect = client.db(dbName).collection("userData");
 
-        const db = await challengeCollect.findOne({
+        const db = await dataCollect.findOne({
             _id: req.body._id
         });
         if (db) {
-            res.status(400).send("Bad request: challenge already exists with id " + req.body.id);
+            res.status(400).send("Bad request: data already exists with id " + req.body.id);
             return;
         }
 
-        let newChallenge = {
+        let newUser = {
             _id: req.body.id,
-            name: req.body.name,
-            points: req.body.points,
-            course: req.body.course,
-            session: req.body.session
+            email: req.body.email,
+            password: req.body.password,
         }
 
-        let insertChallenge = await challengeCollect.insertOne(newChallenge);
+        let insertData = await dataCollect.insertOne(newUser);
 
-        res.status(201).send(`Challenge succesfully saved with name ${req.body.name}`);
+        res.status(201).send(`Data succesfully saved with name ${req.body.name}`);
         return;
 
 
