f805b54302d9f22286cb77b8e03ace4a8731e2d3
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-12-12 13:09:14 +0100

    Changed hash to password

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.post('/userdata/login', async (req, res) => {
 
         if (userDataCollect) {
             //returns true or false
-            const validateHash = await bcrypt.compare(password, userDataCollect.hash);
+            const validateHash = await bcrypt.compare(password, userDataCollect.password);
             if (validateHash) {
                 res.status(200).json('Valid Email and Password!')
             } else {
