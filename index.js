4814fc35234239c6f0ef777cbe460a11151e651e
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-12-12 12:19:51 +0100

    bcrypt added testing hashed passwords

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 //Basic setup done by help from Mike Derycke's youtube
 //Source: https://www.youtube.com/watch?v=p3O6YcUUJmE&t=201s   (Channel: https://www.youtube.com/channel/UChlEg91ycOHnDSicCluhbDA)
+
 //Also used / adapted code and structure from previous exercise: "Full Stack Teamwork"
 //Source: https://github.com/EHB-MCT/web2-groupproject-backend-team-sam
 
+//Registering user and login user, hashing password used youtube video
+//Source: https://www.youtube.com/watch?v=9yIrM7eZwUE   (Channel: https://www.youtube.com/channel/UCDdAZtsEcn3IxBaDVjkjBGg)
+
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
 const {
     MongoClient,
     ObjectId
@@ -10,12 +16,13 @@ const {
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config();
 
 const client = new MongoClient(process.env.FINAL_URL);
 const app = express();
 const port = process.env.PORT || 1337;
 const dbName = "CourseProject";
+//getRandomValues
+var bcrypt = dcodeIO.bcrypt;
 
 //use everything from public folder
 app.use(express.static('public'))
@@ -35,7 +42,7 @@ app.get('/', (req, res) => {
     //status 300 shows that there are multiple options the client can choose from
     //Source: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
     res.status(300).redirect('/api-info.html');
-})
+});
 
 
 //Get userData
@@ -66,40 +73,22 @@ app.get('/userdata/get', async (req, res) => {
     } finally {
         await client.close();
     }
-})
+});
 
 
-//Add UserData
-app.post('/userdata/send', async (req, res) => {
-    if (!req.body.email || !req.body.password) {
-        res.status(400).send("Bad request, missing: email or password!");
-        return;
-    }
 
+app.post('/userdata/register', async (req, res) => {
     try {
-        await client.connect();
-        const dataCollect = client.db(dbName).collection("userData");
-
-        const db = await dataCollect.findOne({
-            _id: req.body._id
+        const {
+            email,
+            password
+        } = req.body;
+        const hash = bcrypt.hash(password, 15);
+        await db(dbName).collection('userData').insert({
+            email: email,
+            hash: hash
         });
-        if (db) {
-            res.status(400).send("Bad request: data already exists with id " + req.body.id);
-            return;
-        }
-
-        let newUser = {
-            _id: req.body.id,
-            email: req.body.email,
-            password: req.body.password,
-        }
-
-        let insertData = await dataCollect.insertOne(newUser);
-
-        res.status(201).send(`Data succesfully saved with email ${req.body.email}`);
-        return;
-
-
+        res.status(200).send("Data hashed and saved succesfully!")
     } catch (err) {
         console.log(err);
         res.status(500).send({
@@ -107,10 +96,54 @@ app.post('/userdata/send', async (req, res) => {
             value: error
         });
     } finally {
+        //Closes the connection if it doesn't work
         await client.close();
     }
 });
 
+
+//Add UserData
+// app.post('/userdata/register', async (req, res) => {
+//     if (!req.body.email || !req.body.password) {
+//         res.status(400).send("Bad request, missing: email or password!");
+//         return;
+//     }
+
+//     try {
+//         await client.connect();
+//         const dataCollect = client.db(dbName).collection("userData");
+
+//         const db = await dataCollect.findOne({
+//             _id: req.body._id
+//         });
+//         if (db) {
+//             res.status(400).send("Bad request: data already exists with id " + req.body.id);
+//             return;
+//         }
+
+//         let newUser = {
+//             _id: req.body.id,
+//             email: req.body.email,
+//             password: req.body.password,
+//         }
+
+//         let insertData = await dataCollect.insertOne(newUser);
+
+//         res.status(201).send(`Data succesfully saved with email ${req.body.email}`);
+//         return;
+
+
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).send({
+//             error: 'Something went wrong',
+//             value: error
+//         });
+//     } finally {
+//         await client.close();
+//     }
+// });
+
 app.listen(port, () => {
     console.log(`API running at at http://localhost:${port}`)
-})
+});
