ca5a58edae02f3dfaaaa7020200aafb0b5767536
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-12-12 16:36:05 +0100

    Testing login

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.post('/userdata/login', async (req, res) => {
 
         if (userDataCollect) {
             //returns true or false
-            const validateHash = await bcrypt.compare(req.body.password, userDataCollect.hash);
+            const validateHash = await bcrypt.compare(req.body.password, userDataCollect.password);
             if (validateHash) {
                 res.status(200).send('Valid Email and Password!')
             } else {
